8fa668335ae46b424aec971de7038fc09d6e4e97
Risqi Romadhoni <risqir57@gmail.com>
2020-12-30 04:18:42 -0500

    add route home

---
+++ config/google.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Application Api Key
+    |--------------------------------------------------------------------------
+    |
+    | URL: https://console.developers.google.com/apis/credentials/key/eb6caf22-e7ce-4b7b-a518-3399914d4dfa?hl=ID&project=pesenduluan
+    |
+    */
+    'api_key' => env('GOOGLE_API_KEY', '')
+];
--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -14,21 +14,30 @@
         <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="{{asset('storage/img/apple-touch-icon-72x72-precomposed.png')}}">
         <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="{{asset('storage/img/apple-touch-icon-114x114-precomposed.png')}}">
         <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="{{asset('storage/img/apple-touch-icon-144x144-precomposed.png')}}">
-    
         <!-- GOOGLE WEB FONT -->
         <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&display=swap" rel="stylesheet">
-    
         <!-- BASE CSS -->
         <link href="{{asset('assets/css/bootstrap_customized.min.css')}}" rel="stylesheet">
         <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
-    
         <!-- SPECIFIC CSS -->
         <link href="{{asset('assets/css/home.css')}}" rel="stylesheet">
-    
         <!-- YOUR CUSTOM CSS -->
         <link href="{{asset('assets/css/custom.css')}}" rel="stylesheet">
+        @yield('styles')
     </head>
     <body class="antialiased">
-
+        @includeIf('partition.frontend.header')
+        {{-- START: Main Views --}}
+        <main>
+            @yield('views')
+        </main>
+        {{-- END: Main Views --}}
+        @includeIf('partition.frontend.footer')
+        <!-- COMMON SCRIPTS -->
+        <script src="{{asset('assets/js/common_scripts.min.js')}}"></script>
+        <script src="{{asset('assets/js/common_func.js')}}"></script>
+        <script src="{{asset('assets/js/validate.js')}}"></script>
+        <!-- Autocomplete -->
+        @yield('scripts')
     </body>
 </html>
